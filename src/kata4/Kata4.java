c5f8dea3c029566f23b56abe2c5a6ccaced7f75d
usuario <usuario@10.22.143.36>
2015-10-28 10:29:49 +0000

    Second commit

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,48 +1,17 @@
 package kata4;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
 
 public class Kata4 {
 
-    public static void main(String[] args) throws IOException {
-
-        String path = "C:\\Users\\usuario\\Documents";
-        File file = new File (path);
+    public static void main(String[] args) throws FileNotFoundException, IOException{
         
-        String from = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Lighthouse.jpg";
-        String to = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Lighthouse (1).jpg";
-
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
-        Date date = new Date();
-        byte[] buffer = new byte[1024];
+        String nameFile="C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\Data\\emailsfilev1.txt";
+        ArrayList <String> mailArray = MailListReader.reader(nameFile);
+        Histogram<String> histo = MailHistogramBuilder.build(mailArray);
         
-        while (true){
-            int read = input.read(buffer);
-            if (read < 0) break;
-            output.write(buffer);
-        }
-        
-        System.out.println(new Date().getTime()-date.getTime()+"msg");
-        input.close();
-        output.flush();
-        output.close();
-        
-        //print(file.listFiles(),"");
-}
-
-    private static void print(File[] listFiles, String indent) {
-        if (listFiles == null) return;
-        for (File listFile : listFiles) {
-            System.out.println(indent+(listFile.isDirectory()?"+":" ")+listFile.getName());
-            if(!listFile.isDirectory() || listFile.isHidden()) continue;
-            print(listFile.listFiles()," ");
-        }
-    }
+        new HistogramDisplay(histo).execute();
+    }   
 }
